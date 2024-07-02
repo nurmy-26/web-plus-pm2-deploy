386ec5c5004afb572f99acadcef79c2268466a57
Nurmy <nurmy26@yandex.ru>
2024-07-02 22:32:12 +0300

    feat: crash-test & README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Деплой приложения на сервер с использованием pm2
 
-Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+IP адрес 158.160.141.223
+Frontend https://nurmy-mesto.nomorepartiesco.ru/
+Backend https://api.nurmy-mesto.nomorepartiesco.ru/
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,6 +18,13 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
